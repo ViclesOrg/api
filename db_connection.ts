3feab788635cc2ee68756e9958fb7d656223d49c
Ilias Elbadaoui <iliassovich@outlook.com>
2024-09-16 23:16:05 +0100

    Integrated azure Database for PostgreSQL

--- db_connection.ts
+++ db_connection.ts
@@ -3,9 +3,16 @@ import { Client } from 'pg';
 export const newClient = ()=>{
     return  new Client({
         user: 'postgres',
-        host: 'localhost',
+        host: 'viclesdev.postgres.database.azure.com',
         database: 'vicles_db',
-        password: '@itf@sk@#98',
+        password: 'AitF@sk@#98',
         port: 5432,
+        ssl: {
+            rejectUnauthorized: false, // Set to true if you want to enforce strict SSL checks
+            // You can also include other SSL options here if needed, such as:
+            // ca: fs.readFileSync('path_to_ca_certificate').toString(),
+            // key: fs.readFileSync('path_to_client_key').toString(),
+            // cert: fs.readFileSync('path_to_client_certificate').toString(),
+          }
     });
 }
